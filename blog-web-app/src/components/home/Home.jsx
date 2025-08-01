92a4f8349a9bc1cc0e743e1bd5c4756c457fa41c
Disha-arch <guptadisha.1009@gmail.com>
2025-08-02 02:25:09 +0530

    added a hero section

--- blog-web-app/src/components/home/Home.jsx
+++ blog-web-app/src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Navbar from "./navbar/Navbar";
+import Hero from "./hero/Hero";
 
 const Home = () => {
   return (
     <div className="main-home-div">
       <Navbar />;
+      <Hero />
     </div>
   );
 };
