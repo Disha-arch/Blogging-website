391d7f60bb488c4f99fa4b1702d5cfbd3ef9be37
Disha-arch <guptadisha.1009@gmail.com>
2025-08-06 06:13:09 +0530

    updated featured.css file

--- blog-web-app/src/components/home/hero/featuredPost/FeaturedPost.jsx
+++ blog-web-app/src/components/home/hero/featuredPost/FeaturedPost.jsx
@@ -6,10 +6,10 @@ import { FaRegHeart } from "react-icons/fa";
 const FeaturedPost = () => {
   return (
     <div>
-      <div>
-        <div>
-          <div>Featured post</div>
-          <div>
+      <div className="featured-image-main-div">
+        <div className="main-text-div">
+          <div className="featured-post-text-div">Featured post</div>
+          <div className="fp-title-para-div">
             <h2>The Art of Mindful Writing : Finding Your Voice</h2>
             <p>
               Discover how mindfulness can transform your writing process and
@@ -17,25 +17,31 @@ const FeaturedPost = () => {
               post , we explore practical techniques
             </p>
           </div>
-          <div>
-            <div>
-              <IoCalendarClearOutline />
+          <div className="date-read-div">
+            <div className="calendar-icon-date-div">
+              <IoCalendarClearOutlines />
               <p>March 15 , 2025</p>
             </div>
             <button>Read More</button>
           </div>
         </div>
-        <div>
-          <div>
-            <div>
+        <div className="image-icons-main-div">
+          <div className="eye-heart-icon-div">
+            <div className="eye-icon-div">
               <FaRegEye />
               <h5>234</h5>
             </div>
-            <div>
+            <div className="heart-icon-div">
               <FaRegHeart />
               <h5>18</h5>
             </div>
-            <img src="featured_post_img.jfif"></img>
+          </div>
+          <div className="featured-image-div">
+            <img
+              src="featured_post_1.jfif"
+              className="featured-image"
+              alt="Featured Image"
+            ></img>
           </div>
         </div>
       </div>
