936b3494ec1d72b62ec1e99701aa46070e2148c0
Disha-arch <guptadisha.1009@gmail.com>
2025-08-07 22:01:22 +0530

    updated recentPost.jsx

--- blog-web-app/src/components/recentPost/RecentPost.jsx
+++ blog-web-app/src/components/recentPost/RecentPost.jsx
@@ -1,46 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./RecentPost.css";
 import { IoCalendarClearOutline } from "react-icons/io5";
 import { IoEyeOutline } from "react-icons/io5";
 import { CiHeart } from "react-icons/ci";
 import { FaRegComment } from "react-icons/fa";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase-config";
 
 const RecentPost = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postsCollection = collection(db, "posts");
+      const postSnapshot = await getDocs(postsCollection);
+      const postList = postSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPosts(postList);
+      console.log("fetched posts :", postList);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
       <div className="recent-post-view-all-button-div">
         <h4>Recent Posts</h4>
         <button>View All</button>
       </div>
-      <div>
-        <div>
-          <div>Photography</div>
-          <h3>10 Tips for Better Blog Photography</h3>
-          <p>
-            Learn how to capture stunning images that complement your written
-            content and enagage your readers.....
-          </p>
-          <div>
-            <IoCalendarClearOutline />
-            <p>March 12 , 2024</p>
-          </div>
-          <div>
-            <div>
-              <div>
-                <IoEyeOutline />
-                <p>156</p>
+      <div className="all-recent-posts-div">
+        <div className="recent-post-indiv-div">
+          {posts.map((post) => (
+            <div key={post.id}>
+              <div className="recent-post-category-div">{post.category}</div>
+              <h3 className="recent-post-title">{post.title}</h3>
+              <p className="recent-post-content">{post.content}</p>
+              <div className="recent-post-date-div">
+                <IoCalendarClearOutline color="rgb(108, 44, 44)" />
+                <p>{new Date(post.date.seconds * 1000).toLocaleDateString()}</p>
               </div>
-              <div>
-                <CiHeart />
-                <p>12</p>
-              </div>
-              <div>
-                <FaRegComment />
-                <p>8</p>
+              <div className="like-view-comm-read-div">
+                <div className="like-view-comm-div">
+                  <div className="views-div">
+                    <IoEyeOutline color="rgb(108, 44, 44)" />
+                    <p>{post.views}</p>
+                  </div>
+                  <div className="likes-div">
+                    <CiHeart color="rgb(108, 44, 44)" />
+                    <p>{post.likes}</p>
+                  </div>
+                  <div className="comments-div">
+                    <FaRegComment color="rgb(108, 44, 44)" />
+                    <p>{post.comments}</p>
+                  </div>
+                </div>
+                <div className="read-div">Read</div>
               </div>
             </div>
-            <div>Read</div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
